8d8054d8e285b90461ed994be06bcf7069b23914
nick <nick@899a1914-839d-7b46-afe5-da559765d5e5>
2008-09-06 17:29:33 +0000

    latest swingcommand
    git-svn-id: https://www.objectdefinitions.com/od-repos/odrepository/trunk@596 899a1914-839d-7b46-afe5-da559765d5e5

---
+++ src/main/java/swingcommand/DefaultAsynchronousCommand.java
@@ -0,0 +1,11 @@
+package swingcommand;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nick
+ * Date: 06-Sep-2008
+ * Time: 14:38:36
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class DefaultAsynchronousCommand extends AbstractAsynchronousCommand<AsynchronousExecution> {
+}
