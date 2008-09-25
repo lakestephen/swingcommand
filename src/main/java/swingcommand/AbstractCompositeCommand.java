19c049db96369eb8777936e37be8cb4e9dad10a9
nick <nick@899a1914-839d-7b46-afe5-da559765d5e5>
2008-09-25 09:59:09 +0000

    latest swingcommand for release 1.2
    git-svn-id: https://www.objectdefinitions.com/od-repos/odrepository/trunk@667 899a1914-839d-7b46-afe5-da559765d5e5

--- src/main/java/swingcommand/AbstractCompositeCommand.java
+++ src/main/java/swingcommand/AbstractCompositeCommand.java
@@ -1,3 +1,13 @@
+/**
+ *  This file is part of ObjectDefinitions SwingCommand
+ *  Copyright (C) Nick Ebbutt September 2009
+ *  Licensed under the Academic Free License version 3.0
+ *  http://www.opensource.org/licenses/afl-3.0.php
+ *
+ *  nick@objectdefinitions.com
+ *  http://www.objectdefinitions.com/swingcommand
+ */
+
 package swingcommand;
 
 import java.util.*;
